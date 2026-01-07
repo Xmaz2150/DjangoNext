07d9e5d98ce1428f91d07d2a4b60a9a71deba267
tmuzi <muzirat0+gh@gmail.com>
2026-01-07 13:16:42 +0200

    Hello Django
    - setup basic api with djang 5.2.10

---
+++ backend/api/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloWorld(APIView):
+    """
+    A class-based view for the hello world endpoint.
+    """
+    def get(self, request, *args, **kwargs):
+        return Response({"message": "Hello, world!"})
